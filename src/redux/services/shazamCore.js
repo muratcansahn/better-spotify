1369b9823c9cbd6cbecd3487b253e2fd1d5a830d
Murat Can ŞAHİN <muratcansahin10@gmail.com>
2022-09-29 16:46:55 +0300

    init && fetch data

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "b77330c3e0msh45c6f54ef5955d1p19f6d4jsnf3111eb84e00"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({
+      query: () => "/charts/world",
+    }),
+  }),
+});
+export const { useGetTopChartsQuery } = shazamCoreApi;
