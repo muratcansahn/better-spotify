8d6eff8e6118ecf73891ac0aae5607d7854760ed
Murat Can ŞAHİN <muratcansahin10@gmail.com>
2022-09-30 14:25:17 +0300

    add song card component

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,17 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import { FaPauseCircle, FaPlayCircle } from "react-icons/fa";
+
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) =>
+  isPlaying && activeSong?.title === song.title ? (
+    <FaPauseCircle
+      size={35}
+      className="text-gray-300 cursor-pointer"
+      onClick={handlePause}
+    />
+  ) : (
+    <FaPlayCircle
+      className="text-4xl text-white cursor-pointer"
+      onClick={handlePlay}
+    />
+  );
 
 export default PlayPause;
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants.js";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, isError } = useGetTopChartsQuery();
   console.log(data, isError, isFetching);
+
+  if (isFetching) {
+    return <Loader title=" Loading songs ..." />;
+  }
+  if (isError) {
+    return <Error title="Something went wrong" />;
+  }
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
@@ -21,8 +31,15 @@ const Discover = () => {
         </select>
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
-          <SongCard key={song.key} song={song} i={i} />
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            i={i}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+          />
         ))}
       </div>
     </div>
